63ba0e0e7bcd6752e406d2e625169778ae2c583c
Sky Jillain <68349004+huijillain@users.noreply.github.com>
2020-09-25 19:28:18 -0600

    added MVC file

---
+++ PriceQuotationMVC/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PriceQuotationMVC.Models;
+
+namespace PriceQuotationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]                 // when user open URL, GET will open
+        public IActionResult Index()
+        {
+            ViewBag.DiscountAmount = 0;    // assign initial value to 0
+            ViewBag.Total = 0;
+            return View();     // return view from homepage
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)   // this Index will take FutureValueModel, process data here
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DiscountAmount = model.CalculateDiscountAmount();
+                ViewBag.Total = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.DiscountAmount = 0;
+                ViewBag.Total = 0;
+            }
+            return View(model);
+        }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ PriceQuotationMVC/Models/PriceQuotationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotationMVC.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter the Subtotal amount!")]
+        [Range(0, 10000, ErrorMessage = "Subtotal amount must be greater than zero.")]
+        public decimal Subtotal { get; set; }   // property
+        [Required(ErrorMessage = "Please enter the discount percentage!")]
+        [Range(0, 100, ErrorMessage = "Discount percentage must be in range 0 to 100")]
+        public decimal DiscountPercent { get; set; }  // property      
+        public decimal CalculateDiscountAmount()            // method
+        {
+            decimal discountAmount = Subtotal * DiscountPercent / 100;
+            return discountAmount;
+        }
+        public decimal CalculateTotal()            // method
+        {
+            decimal discountAmount = Subtotal * DiscountPercent / 100;
+            decimal total = Subtotal - discountAmount;
+            return total;
+        }
+    }
+}
